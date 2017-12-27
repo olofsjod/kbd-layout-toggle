d081dc9e96c3bce5f3d0c61735f47d6046a36660
Olof Sjödin <me@olofsjodin.se>
2017-12-27 13:22:03 +0100

    add main and toggle for keyboard layout changer

--- kbd-layout-toggle.py
+++ kbd-layout-toggle.py
@@ -1,4 +1,5 @@
 import subprocess
+import argparse
 
 def getCurrentKbdLayout():
     s = subprocess.run(["setxkbmap", "-query"], stdout=subprocess.PIPE)
@@ -9,18 +10,32 @@ def getCurrentKbdLayout():
 
 def changeKbdLayout(layout):
     s = subprocess.run(["setxkbmap", layout], stdout=subprocess.PIPE)
+
+def toggle(keyboard_layouts):
+    cur_layout_str = getCurrentKbdLayout()
+    cur_layout_index = keyboard_layouts.index(cur_layout_str)
+
+    next_layout_index = (cur_layout_index + 1) % len(keyboard_layouts)
+    next_layout_str = keyboard_layouts[next_layout_index]
+    
+    changeKbdLayout(next_layout_str)
     
 
 def main():
-    currentLayout = getCurrentKbdLayout()
-
-    if currentLayout == "se":
-        changeKbdLayout("se-svorak-custom")
-    else:
-        changeKbdLayout("se")
-
-    print(getCurrentKbdLayout())
-
+    parser = argparse.ArgumentParser()
     
+    parser.add_argument("--print", help="print the current keyboard layout", action="store_true")
+    parser.add_argument("--change-layout", help="change the keyboard layout to other layout", nargs=1)
+    parser.add_argument("--toggle", help="change the keyboard layout between se and se-svorak-custom", action="store_true")
+    
+    args = parser.parse_args()
+
+    if args.print:
+        print(getCurrentKbdLayout())
+    elif args.toggle:
+        toggle(["se", "se-svorak-custom"])
+    elif len(args.change_layout) > 0:
+        new_layout = args.change_layout[0]
+        changeKbdLayout(new_layout)
 
 main()
