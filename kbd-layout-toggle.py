648099a6a74d73cfe6a31782fad9336d93018246
Olof Sjödin <me@olofsjodin.se>
2018-05-05 09:43:33 +0200

    change code to make it compatible with the built in dvorak support

--- kbd-layout-toggle.py
+++ kbd-layout-toggle.py
@@ -1,21 +1,74 @@
 import subprocess
 import argparse
 
-def getCurrentKbdLayout():
+def getXKBmapQuery():
     s = subprocess.run(["setxkbmap", "-query"], stdout=subprocess.PIPE)
     output_str = s.stdout.decode("utf-8")
     output_arr = output_str.split("\n")
+    output_arr = output_arr[:-1]
 
-    return output_arr[2].split(":     ")[1]
+    ret = {}
+    for line in output_arr:
+        temp = line.replace(" ", "") #remove spaces
+        temp = temp.split(":")
+        key = temp[0]
+        val = temp[1]
+        
+        ret[key] = val
 
-def changeKbdLayout(layout):
-    msg = "Changed layout to " + layout
-    subprocess.run(["notify-send", msg], stdout=subprocess.PIPE)
-    subprocess.run(["setxkbmap", layout], stdout=subprocess.PIPE)
+    return ret
+
+def printCurrentKbdLayout():
+    q = getXKBmapQuery()
     
+    ret = ""
+    if "layout" in q:
+        ret += q["layout"]
+
+    if "variant" in q:
+        ret += "/" + q["variant"]
+    else:
+        ret += "/qwerty"
+
+    print(ret)
+
+
+def changeKbdLayout(k):
+    msg = "Changed layout to " + str(k)
+
+    parameters = []
+
+    if "layout" in k:
+        parameters.extend(["-layout", k["layout"]])
+    if "variant" in k:
+        parameters.extend(["-variant", k["variant"]])
+
+    cmd = ["setxkbmap"]
+    cmd.extend(parameters)
+    
+    subprocess.run(["notify-send", msg], stdout=subprocess.PIPE)
+    subprocess.run(cmd, stdout=subprocess.PIPE)
+
+def isDvorak():
+    q = getXKBmapQuery()
+    if "variant" in q:
+        if q["variant"] == "dvorak":
+            return True
+    return False
+
+def toggleDvorak():
+    is_dvorak = isDvorak()
+
+    if is_dvorak:
+        k = {"layout": "se", "variant" : ""}
+        changeKbdLayout(k)
+    else:
+        k = {"layout": "se", "variant" : "dvorak"}
+        changeKbdLayout(k)
+        
 
 def toggle(keyboard_layouts):
-    cur_layout_str = getCurrentKbdLayout()
+    cur_query = getXKBmapQuery()
     cur_layout_index = keyboard_layouts.index(cur_layout_str)
 
     next_layout_index = (cur_layout_index + 1) % len(keyboard_layouts)
@@ -28,17 +81,20 @@ def main():
     parser = argparse.ArgumentParser()
     
     parser.add_argument("--print", help="print the current keyboard layout", action="store_true")
-    parser.add_argument("--change-layout", help="change the keyboard layout to other layout", nargs=1)
+    parser.add_argument("--layout", help="change the keyboard layout to other layout", nargs=1)
+ 
     parser.add_argument("--toggle", help="change the keyboard layout between se and se-svorak-custom", action="store_true")
     
     args = parser.parse_args()
 
     if args.print:
-        print(getCurrentKbdLayout())
+        printCurrentKbdLayout()
     elif args.toggle:
-        toggle(["se", "se-svorak-custom"])
-    elif len(args.change_layout) > 0:
-        new_layout = args.change_layout[0]
-        changeKbdLayout(new_layout)
+        toggleDvorak()
+    elif len(args.layout) > 0:
+        new_layout = args.layout[0]
+        new_variant = args.layout[1]
+        l = {"layout" : new_layout, "variant" : new_variant}
+        changeKbdLayout(l)
 
 main()
